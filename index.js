e0c04be4b110e82ad73ae7fc384a35ab00e49739
ashish.musani <ashish_musani@ymail.com>
2021-10-27 18:48:46 -0400

    Added test model/schema. Created 'Add Test for a Patient' endpoint.

--- index.js
+++ index.js
@@ -19,21 +19,30 @@ var patientSchema = new mongoose.Schema({
 });
 var Patient = mongoose.model('Patient', patientSchema)
 
+var testSchema = new mongoose.Schema({
+    patientId: String,
+    date: String,
+    time: String,
+    typeOfData: String,
+    value: String
+});
+var Test = mongoose.model('Test', testSchema)
+
 var server = restify.createServer({
     name: "'Health Records (MAPD713-Milestone2)'"
 });
 server.listen(PORT, () => {
     console.log("Server %s is listening on port %s", server.name, PORT);
     console.log("========Resources========");
-    console.log("/patients");
-    console.log("/patients/:id");
-    console.log("/patients");
-    console.log("/patients/:id/tests");
+    console.log("/patients   [GET,POST]");
+    console.log("/patients/:id   [GET]");
+    console.log("/patients/:id/tests   [GET,POST]");
     console.log("=========================")
 })
 server.use(restify.plugins.fullResponse());
 server.use(restify.plugins.bodyParser());
 
+//  List All Patient Info
 server.get('/patients', (req,res) => {
     Patient.find({}, (err, patients) => {
         console.log(patients)
@@ -41,6 +50,7 @@ server.get('/patients', (req,res) => {
     })
 })
 
+//  Add Patient Info
 server.post('/patients', (req,res) => {
 
     const {firstName, lastName, address, age, birthDate, department, doctor } = req.body;
@@ -69,6 +79,7 @@ server.post('/patients', (req,res) => {
     }
 })
 
+//  View Patient Info
 server.get('/patients/:id', async (req,res) => {
     try {
         const patientId = req.params?.id;
@@ -89,3 +100,44 @@ server.get('/patients/:id', async (req,res) => {
         return res.send(500, `There was an error finding the patient. ${err}`);
     }
 })
+
+//  Add Test for a Patient
+server.post('/patients/:id/tests', (req,res) => {
+    const patientId = req.params?.id;
+    if(patientId){
+        if(!req.body){
+            res.send(400, "Required data missing in request body")
+            return
+        }
+        var patient = Patient.findById(patientId, (err, patient) => {
+            if(err){
+                return res.send(404, 'Could not find a patient with this ID.');
+            } else {
+                const {date, time, typeOfData, value} = req.body;
+                if(date && time && typeOfData && value){
+                    var newTest = new Test({
+                        patientId: patientId,
+                        date: date,
+                        time: time,
+                        typeOfData: typeOfData,
+                        value: value                
+                    })
+                    newTest.save((err, result) => {
+                        if(err){
+                            res.send(500, "There was some problem fulfilling the request. Please try again.");
+                        } else {
+                            res.send(201, result);
+                        }
+                    })
+                } else {
+                    res.send(400, "One or more fields are missing values")
+                }
+
+            }
+        });
+    } else {
+        return res.send(400, 'Patient ID is required.');
+    }
+})
+
+//  View Tests for a Patient
